dd057aec140aad94b70efd92cfa36c6519c7b2c4
Artem <armlynskii@gmail.com>
2022-10-17 10:30:26 +0700

    add ads remove function

--- demoqa_tests/model/pages/registration_form.py
+++ demoqa_tests/model/pages/registration_form.py
@@ -14,9 +14,9 @@ from selenium.webdriver.common.keys import Keys
 @allure.step('Открываем страницу регистрации')
 def given_opened():
     browser.open('/automation-practice-form')
-    ads = ss('[id^=google_ads][id$=container__]')
-    if ads.with_(timeout=10).wait.until(have.size_greater_than_or_equal(3)):
-        ads.perform(command.js.remove)
+    ads = browser.all('[id^=google_ads][id$=container__]')
+    ads.should(have.size_greater_than_or_equal(3))
+    ads.perform(command.js.remove)
 
 
 @allure.step('Заполняем имя')
