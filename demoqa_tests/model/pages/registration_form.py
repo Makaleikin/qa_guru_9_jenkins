f165f6f345f8b83233e69d340f06d31a303ec0b8
Artem <armlynskii@gmail.com>
2022-10-19 11:50:31 +0700

    add func scroll to element

--- demoqa_tests/model/pages/registration_form.py
+++ demoqa_tests/model/pages/registration_form.py
@@ -16,7 +16,7 @@ def given_opened():
 
 
 def remove_advertisement():
-    ads = browser.all('[id^=google_ads][id$=container__]')
+    ads = browser.all('[id^=google_ads_iframe][id$=container__]')
     #if ads.wait.until(have.size_greater_than_or_equal(>2)):
     ads.perform(command.js.remove)
 
