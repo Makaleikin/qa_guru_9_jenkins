f5d063050842ad0e1c1274ba6b58e40789638dfb
Artem <armlynskii@gmail.com>
2022-10-24 16:07:21 +0700

    add func scroll to element

--- demoqa_tests/model/contols/dropdown.py
+++ demoqa_tests/model/contols/dropdown.py
@@ -1,9 +1,9 @@
 from selene import have
 from selene.support.shared import browser
-from selene import have, command
+
 
 def select(element, option):
-    element.perform(command.js.click)
+    element.click()
     browser.all('[id^=react-select][id*=-option-]').by(
         have.exact_text(option)
-    ).first.perform(command.js.click)
+    ).first.click()
